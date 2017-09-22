c044213ff3e1a77986efbead0120bf75d5f7f10d
Paul Cevallos <pcevallos@digitalstrategy.com.ec>
2017-09-22 17:15:07 -0500

    Se agregó funcionalidad para eliminación y descarga de archivos desde el cliente al servicio web.

--- swTH/bd.swth.servicios/Interfaces/IUploadFileService.cs
+++ swTH/bd.swth.servicios/Interfaces/IUploadFileService.cs
@@ -1,4 +1,6 @@
-﻿using bd.swth.entidades.Utils;
+﻿using bd.swth.entidades.ObjectTransfer;
+using bd.swth.entidades.Utils;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -10,5 +12,7 @@ namespace bd.swth.servicios.Interfaces
    public interface IUploadFileService
     {
         Task<bool> UploadFile(byte[] file, string folder, string fileName, string extension);
+        bool DeleteFile(string folder, string fileName, string extension);
+        DocumentoInstitucionalTransfer GetFile(string folder, string fileName, string extension);
     }
 }
