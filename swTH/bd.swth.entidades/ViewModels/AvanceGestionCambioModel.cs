9c76743f61676cfe410933b935573211cff882f5
Adriana Cristina Espinosa Román <aespinosa@digitalstrategy.com.ec>
2017-11-06 11:23:28 -0500

    Se ha cambiado la clase ActividadesGestionCambio Se ha creado los View Model ActividadesGestionCambioIndex y AvanceGestionCambioModel Se ha creado los controladores Planes, Avctividades y Avance Gestion Cambio

---
+++ swTH/bd.swth.entidades/ViewModels/AvanceGestionCambioModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.swth.entidades.ViewModels
+{
+    public partial class AvanceGestionCambioModel
+    {
+        public decimal Suma { get; set; }
+    }
+}
