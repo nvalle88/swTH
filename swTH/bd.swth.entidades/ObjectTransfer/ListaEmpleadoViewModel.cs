838d3e03db5d85bbee6154cda08e752c55a0c9fb
Paul Cevallos <pcevallos@digitalstrategy.com.ec>
2017-11-10 14:31:53 -0500

    Se agregaró la funcionalidad de listar empleados.

---
+++ swTH/bd.swth.entidades/ObjectTransfer/ListaEmpleadoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bd.swth.entidades.ObjectTransfer
+{
+    public class ListaEmpleadoViewModel
+    {
+        public int IdEmpleado { get; set; }
+        public string NombreApellido { get; set; }
+        public string Identificacion { get; set; }
+        public string TelefonoPrivado { get; set; }
+        public string CorreoPrivado { get; set; }
+    }
+}
