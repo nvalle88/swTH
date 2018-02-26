05dfd190e4bdd17d4cf7705372a5dd4144abb893
josue <jcatota@digitalstrategy.com.ec>
2018-02-26 09:40:12 -0500

    normalización, corrección, (revisar tipos enfermedades)

--- swTH/bd.swth.web/Controllers/API/TiposEnfermedadesController.cs
+++ swTH/bd.swth.web/Controllers/API/TiposEnfermedadesController.cs
@@ -287,7 +287,7 @@ namespace bd.swth.web.Controllers.API
                 return new Response
                 {
                     IsSuccess = false,
-                    Message = Mensaje.Error,
+                    Message = Mensaje.BorradoNoSatisfactorio,
                 };
             }
         }
--- swTH/bd.swth.web/Controllers/API/TitulosController.cs
+++ swTH/bd.swth.web/Controllers/API/TitulosController.cs
@@ -318,7 +318,7 @@ namespace bd.swth.web.Controllers.API
                 return new Response
                 {
                     IsSuccess = false,
-                    Message = Mensaje.Error,
+                    Message = Mensaje.BorradoNoSatisfactorio,
                 };
             }
         }
