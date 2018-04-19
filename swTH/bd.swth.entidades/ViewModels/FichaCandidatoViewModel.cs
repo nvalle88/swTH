2260d33f2a7f00cb83fcf957ef1f5900656069d3
Néstor Valle <nestorvalle880416@gmail.com>
2018-04-19 10:40:00 -0500

    Funcionalidad ficha empleado

---
+++ swTH/bd.swth.entidades/ViewModels/FichaCandidatoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace bd.swth.entidades.ViewModels
+{
+   public class FichaCandidatoViewModel
+    {
+
+        public int IdCandidato { get; set; }
+
+        public int IdPersona { get; set; }
+
+        public string Identificacion { get; set; }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string TelefonoPrivado { get; set; }
+
+        public string TelefonoCasa { get; set; }
+
+        public string CorreoPrivado { get; set; }
+    }
+}
