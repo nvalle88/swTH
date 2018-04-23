876762b4e15f6e75233bd2e4f6091fde9739ed77
Néstor Valle <nestorvalle880416@gmail.com>
2018-04-23 17:50:44 -0500

    Entidades de configuración de nómina

---
+++ swTH/bd.swth.entidades/Negocio/ConfiguracionNomina/ProcesoNomina.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bd.swth.entidades.Negocio
+{
+    public partial class ProcesoNomina
+    {
+        public ProcesoNomina()
+        {
+            ConceptoNomina = new HashSet<ConceptoNomina>();
+        }
+
+        public int IdProceso { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<ConceptoNomina> ConceptoNomina { get; set; }
+    }
+}
---
+++ swTH/bd.swth.entidades/Negocio/ConfiguracionNomina/TeconceptoNomina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace bd.swth.entidades.Negocio
+{
+    public partial class TeconceptoNomina
+    {
+        public int IdTeconcepto { get; set; }
+        public int IdConcepto { get; set; }
+
+        public virtual ConceptoNomina IdConceptoNavigation { get; set; }
+    }
+}
